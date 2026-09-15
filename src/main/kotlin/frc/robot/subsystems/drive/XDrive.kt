d46eb47870ee6cb4f188900a37feb0eca752e52e
whiteswordv <143036168+whiteswordv@users.noreply.github.com>
2026-09-15 16:59:10 -0400

    base XDrive
    probably needs fixing

--- src/main/kotlin/frc/robot/subsystems/drive/XDrive.kt
+++ src/main/kotlin/frc/robot/subsystems/drive/XDrive.kt
@@ -1,9 +1,34 @@
+import com.revrobotics.spark.SparkLowLevel.MotorType
+import com.revrobotics.spark.SparkMax
+import edu.wpi.first.math.kinematics.ChassisSpeeds
+import edu.wpi.first.units.Units
+import edu.wpi.first.units.measure.Angle
+import kotlin.math.atan2
+
 class XDrive {
 
+    private val m_frontLeftNeo: SparkMax = SparkMax(0, MotorType.kBrushless)
+    private val m_frontRightNeo: SparkMax = SparkMax(0, MotorType.kBrushless)
+    private val m_backLeftNeo: SparkMax = SparkMax(0, MotorType.kBrushless)
+    private val m_backRightNeo: SparkMax = SparkMax(0, MotorType.kBrushless)
 
     constructor() {
         
     }
 
+    public fun drive(speeds: ChassisSpeeds) {
+        val angle: Angle = Angle.ofBaseUnits(atan2(speeds.vxMetersPerSecond, speeds.vyMetersPerSecond), Units.Radians)
 
+        val rotation: Double = angle.`in`(Units.Radians) + speeds.omegaRadiansPerSecond;
+
+        // front velocity
+        val frontLeftVelocity: Double = speeds.vxMetersPerSecond + speeds.vyMetersPerSecond + rotation;
+        val frontRightVelocity: Double = speeds.vxMetersPerSecond - speeds.vyMetersPerSecond + rotation;
+
+        // back velocity
+        val backLeftVelocity: Double = -speeds.vxMetersPerSecond - speeds.vyMetersPerSecond - rotation;
+        val backRightVelocity: Double = -speeds.vxMetersPerSecond + speeds.vyMetersPerSecond - rotation;
+
+
+    }
 }
