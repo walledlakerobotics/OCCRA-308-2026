85863b32a3d7661b4052815b459f98664900a18c
whiteswordv <143036168+whiteswordv@users.noreply.github.com>
2026-09-15 16:39:11 -0400

    kotlin for life

---
+++ src/main/kotlin/frc/robot/Main.kt
@@ -0,0 +1,26 @@
+package frc.robot
+
+import edu.wpi.first.hal.FRCNetComm
+import edu.wpi.first.hal.HAL
+import edu.wpi.first.wpilibj.RobotBase
+
+/**
+ * Do NOT add any static variables to this class, or any initialization at all.
+ * Unless you know what you are doing, do not modify this file except to
+ * change the parameter class to the startRobot call.
+ */
+object Main {
+    /**
+     * Main initialization function. Do not perform any initialization here.
+     *
+     *
+     * If you change your main robot class, change the parameter type.
+     */
+    @JvmStatic
+    fun main(args: Array<String>) {
+        RobotBase.startRobot {
+            HAL.report(FRCNetComm.tResourceType.kResourceType_Language, FRCNetComm.tInstances.kLanguage_Kotlin)
+            Robot()
+        }
+    }
+}
