0033d4ab60c4ff40d730a34256c0406efeb82031
whiteswordv <143036168+whiteswordv@users.noreply.github.com>
2026-09-25 22:03:00 -0400

    buffer patch

--- src/main/kotlin/frc/robot/subsystems/drivetrain/DriveOdometry.kt
+++ src/main/kotlin/frc/robot/subsystems/drivetrain/DriveOdometry.kt
@@ -5,24 +5,26 @@ import edu.wpi.first.math.geometry.Rotation2d
 import edu.wpi.first.math.geometry.Translation2d
 import edu.wpi.first.wpilibj.Notifier
 import edu.wpi.first.wpilibj.SerialPort
-import java.io.ByteArrayInputStream
 import java.nio.ByteBuffer
+import java.nio.ByteOrder
 
 
 class DriveOdometry(portDir: String) : AutoCloseable {
     // port serial instance
     private val mSerialPort = SerialPort(115200, SerialPort.Port.kUSB, 8, SerialPort.Parity.kNone, SerialPort.StopBits.kOne)
 
-    private var mByteBuffer: ByteBuffer = ByteBuffer.allocate(0)
+    private var mByteBuffer: ByteBuffer = ByteBuffer.allocate(Float.SIZE_BYTES * 6)
 
     private val mNotifier = Notifier({
         synchronized(mByteBuffer) {
             val bytes = mSerialPort.read(Float.SIZE_BYTES * 6)
-            mByteBuffer = ByteBuffer.wrap(bytes)
+            mByteBuffer.clear()
+            mByteBuffer.put(bytes)
         }
     })
 
     init {
+        mByteBuffer.order(ByteOrder.LITTLE_ENDIAN)
         mNotifier.startPeriodic(0.01)
     }
 
